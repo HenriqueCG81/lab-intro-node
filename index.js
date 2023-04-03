79b289489accae3a019bd5ef026315786bd14fec
HenriqueCG81 <123783235+HenriqueCG81@users.noreply.github.com>
2023-04-03 15:56:29 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((prev, curr) => {
+      return prev + curr;
+    }, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
